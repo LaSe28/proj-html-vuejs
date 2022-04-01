89766f1aa24b30ef7f35ce944bf693b7f9e2cb7b
LaSe28 <mattia.las28@gmail.com>
2022-04-01 17:34:34 +0200

    end layout header

--- src/components/ProjHeader.vue
+++ src/components/ProjHeader.vue
@@ -1,13 +1,13 @@
 <template>
 <header>
-  <div>
+  <div class="nav">
     <img src="../assets/img/theme_eduprime_logo.png" alt="">
-  </div>
-  <div>
-    <ul>
-      <li v-for="(ele, index) in navMenu" :key="index">{{ele.name}}</li>
-    </ul>
-    <button>VIEV COURSES</button>
+    <div class="menu-text">
+      <ul>
+        <li v-for="(ele, index) in navMenu" :key="index">{{ele.name}}</li>
+      </ul>
+    </div>
+    <button class="btn btn-y">VIEW COURSES</button>
   </div>
   <MainSec1/>
 </header>
@@ -61,9 +61,29 @@ export default {
 
 <style scoped lang="scss">
 @import '../assets/styles/partials/_variables.scss';
+@import '../assets/styles/partials/_general.scss';
 
 header{
   background-color: $main-red;
+  color: white;
+}
+.nav{
+  display: flex;
+  align-items: center;
+  img{
+    height: 100px;
+    margin-right: auto;
+  }
+  ul{
+    list-style: none;
+    display: flex;
+    li{
+      margin: 1rem;
+    }
+  }
+  .menu-text{
+    display: flex;
+  }
 }
 
 </style>
--- src/components/ProjMain1.vue
+++ src/components/ProjMain1.vue
@@ -1,9 +1,13 @@
 <template>
 <div class="container">
-  <h1>Key to your success</h1>
-  <p>EduPrime is the most versatile WordPress THeme for educational purposes, showcasing universities, courses, secondary schools etc.</p>
-  <button>search courses</button>
-  <button>Apply for university</button>
+  <div class="centered mt-2 pt-5 pb-10">
+    <h1>Key to your success</h1>
+    <p class="mb-2 mt-2">EduPrime is the most versatile WordPress THeme for educational purposes, showcasing universities, courses, secondary schools etc.</p>
+    <div>
+      <button class="mt-1 mr-1 btn btn-y">search courses</button>
+      <button class="mt-1 btn">Apply for university</button>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -15,6 +19,11 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '../assets/styles/partials/_general.scss'
+@import '../assets/styles/partials/_general.scss';
+
+h1{
+  font-size: 3rem;
+  font-weight: bold;
+}
 
 </style>
